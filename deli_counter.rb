82e708b57ffb28bebfe731159010cc575cd1dad7
Jeff Johnson <synotik@hotmail.com>
2020-11-21 08:51:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,11 +4,15 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else
+<<<<<<< HEAD
     output = "The line is currently: "
     array.each_with_index do |name, index|
       output << "#{index+1}. #{name} "
     end
     puts output.strip
+=======
+    puts "Hello."
+>>>>>>> 54bc3585adb288f6d516085d7d4d2d7281a03161
   end
 end
 
@@ -16,6 +20,7 @@ def take_a_number(array, name)
   array << name
   puts "Welcome, #{name}. You are number #{array.find_index(name) + 1} in line."
 end
+<<<<<<< HEAD
 
 def now_serving(array)
   if array.empty?
@@ -25,3 +30,5 @@ def now_serving(array)
     array.shift
   end
 end
+=======
+>>>>>>> 54bc3585adb288f6d516085d7d4d2d7281a03161
